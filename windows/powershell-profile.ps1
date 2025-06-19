4f3ade2d2cf63ff42bfb4b3f79e1d172c9857bad
Vikram <vikramjit360@gmail.com>
2025-06-20 02:02:32 +0530

    add configs for windows

---
+++ windows/powershell-profile.ps1
@@ -0,0 +1,6 @@
+oh-my-posh init pwsh --config "C:\Users\vikra\OneDrive\Desktop\Random  Stuff\Oh-my-posh themes\cloud-native-azure.omp.json" | Invoke-Expression
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
