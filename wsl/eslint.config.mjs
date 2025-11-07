26e02f5e133e662f4030ed0a905f8c38c7eaf325
vikram(wsl) <vikramjit360@gmail.com>
2025-11-07 11:48:58 +0530

    chore: Update global eslint config

--- wsl/eslint.config.mjs
+++ wsl/eslint.config.mjs
@@ -1,37 +1,37 @@
+import js from "@eslint/js";
 import globals from "globals";
-import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default [
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  pluginReactConfig,
-  { files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"] },
+export default defineConfig([
   {
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-      globals: { ...globals.browser, ...globals.node },
-    },
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
+  globalIgnores([
+    "!node_modules/", // unignore `node_modules/` directory
+    "node_modules/*", // ignore its content
+    "build/**/*", // ignore all contents in and under `build/` directory but not the `build/` directory itself
+    "!build/test.js", // unignore `!build/test.js`
+  ]),
   { settings: { react: { version: "detect" } } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
     rules: {
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/require-await": "warn",
     },
   },
-  {
-    files: ["**/*.js", "**/*.jsx"],
-    rules: {
-      "@typescript-eslint/no-require-imports": 0,
-    },
-  },
-];
+]);
